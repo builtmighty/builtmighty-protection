1bf2b615882b5292819caad330dc629ea2503bb8
Tyler Johnson <tyler@builtmighty.com>
2024-01-31 21:32:12 +0000

    (main) ✨ Add class for assessing orders

---
+++ classes/class-assess.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Assess.
+ * 
+ * A set of rules to assess and rate orders.
+ * 
+ * @package Built Mighty Protection
+ * @since   1.0.0
+ */
+namespace BuiltMightyProtection;
+class builtDetection {
+
+    /**
+     * Construct.
+     * 
+     * @since   1.0.0
+     */
+    
+
+}
